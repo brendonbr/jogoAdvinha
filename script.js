93a2900c1804e6c0bbbb59017c83985572911165
brendonbr <brendonbr15@gmail.com>
2025-06-03 17:31:58 -0300

    finalizacao de estilo do jogo

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const geradorNumerico = () => {
     const min = Math.ceil(1);
     const max = Math.floor(100);
     return Math.floor(Math.random() * (max - min + 1) + min);
- } ;
+};
 
-const getElement = (id) => document.getElementById(id) ;
+const getElement = (id) => document.getElementById(id);
 const numerosPalpitados = [];
 
 let jogadas = 0;
@@ -16,27 +16,25 @@ let totalChances = 10;
 let numeroGerado = geradorNumerico();
 
 
-function chutar(){
+function chutar() {
     getElement("nPalpite").value = geradorNumerico();
 };
 
 
 
-function getFeedback(nGerado, nPalpitado){
 
-    if (nGerado === nPalpitado){
-        return "Parabens, você acertou";
+
+
+function jogar() {
+
+    console.log(numeroGerado)
+
+    if (jogadas >= totalChances) {
+        placarFeedback.textContent = "Fim de jogo, o número gerado era " + valorNPalpitado;
+        return;
     }
-
-    else{
-        return `O numero secreto é ${nGerado>nPalpitado?"maior":"menor"} que  ${nPalpitado}`
-    }
-}
-
-function jogar(){
 
     jogadas++;
-
     let chancesRestantes = totalChances - jogadas;
 
 
@@ -46,34 +44,41 @@ function jogar(){
     const nPalpitado = getElement("nPalpite");
     let valorNPalpitado = parseInt(nPalpitado.value)
 
-    
-    if (chancesRestantes >= 0){
 
 
-        if (valorNPalpitado >= 1 && valorNPalpitado <= 100){
-            
-            numerosPalpitados.push(valorNPalpitado);
-        
-            placarFeedback.textContent = getFeedback(numeroGerado,valorNPalpitado);
-
-            contadorChances.textContent = chancesRestantes;
-
-            placarPalpites.textContent = numerosPalpitados.join(", ");
-            
-            nPalpitado.value = "";
-        } 
-
-        else{
-            alert("valor deve ser entre 1 e 100");
-        }
 
 
-    }
-
-    if (chancesRestantes === 0){
-        placarFeedback.textContent = "Fim de jogo, o número gerado era " + valorNPalpitado;
+    if (isNaN(valorNPalpitado) || valorNPalpitado < 1 || valorNPalpitado > 100) {
+        alert("O valor deve ser entre 1 e 100");
         return;
     }
 
+
+
+
+
+    numerosPalpitados.push(valorNPalpitado);
+
+    placarFeedback.textContent = `O numero secreto é ${numeroGerado > valorNPalpitado ? "maior" : "menor"} que  ${valorNPalpitado}`
+
+
+    contadorChances.textContent = chancesRestantes;
+
+    placarPalpites.textContent = `[ ${numerosPalpitados.join(", ")} ]`;
+
+    nPalpitado.value = "";
+
+    if (numeroGerado == valorNPalpitado) {
+        placarFeedback.textContent = ("Parabêns, você acertou").toUpperCase();
+        return;
+    };
+
+
+    if (chancesRestantes === 0) {
+        placarFeedback.textContent = "Fim de jogo, o número gerado era " + numeroGerado;
+     
+    }
+
+
 }
- 
+
